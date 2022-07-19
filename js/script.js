680a7ee1e7653a16ac29e3b053924da5caa68f08
Martín Devita <martindevita23@gmail.com>
2022-07-19 19:53:19 -0300

    DOM

--- js/script.js
+++ js/script.js
@@ -1,52 +1,35 @@
 class Remera {
-    constructor(modelo, talle, stock, precio){
+    constructor(id, modelo, talle, stock, precio, img) {
+        this.id = id
         this.modelo = modelo
         this.talle = talle
         this.stock = stock
         this.precio = precio
+        this.img = img
     }
 }
 
-const remera1 = new Remera ("la creacion", "XL", 10, 2500)
-const remera2 = new Remera ("la ultima cena", "L", 12, 2400)
-const remera3 = new Remera ("la noche estrellada", "M", 8, 2000)
-const remera4 = new Remera ("la joven de la perla", "XL", 15, 2700)
+const remera1 = new Remera(1, "La creación", "XL", 10, 4000,)
+const remera2 = new Remera(2, "La Última Cena", "L", 12, 4000)
+const remera3 = new Remera(3, "La Noche Estrellada", "M", 8, 4000)
+const remera4 = new Remera(4, "La Joven De La Perla", "XL", 15, 4000)
 
 const remeras = [remera1, remera2, remera3, remera4]
 
-let continuar, modeloSeleccion, modeloComprar, acumulador, cantidadAComprar
+const divRemeras = document.getElementById("remeras")
 
-do{
-    do{
-        modeloSeleccion = prompt(`Ingrese el modelo que desea comprar
-                            1- la creacion
-                            2- la ultima cena
-                            3- la noche estrellada
-                            4- la joven de la perla`).toLowerCase()
-
-        modeloComprar = remeras.find(remera => remera.modelo == modeloSeleccion)
-
-        if(modeloComprar == undefined){
-            alert("Modelo no encontrado")
-        }else{
-            console.log(modeloComprar)
-        }
-
-    } while((modeloSeleccion.length == 0) || (modeloComprar.length == 0))
-
-    do{
-        cantidadAComprar = parseInt(prompt("Ingrese cantidad de remeras que desea comprar"))
-        if(cantidadAComprar <= 0){
-            alert("ingrese una cantidad válida")
-        }else{
-            alert(`usted va a compar ${cantidadAComprar} remeras`)
-        }
-
-    }while (isNaN(cantidadAComprar))
-
-    alert(`usted eligió ${modeloSeleccion}`)
-
-    continuar = prompt("Desea finalizar la compra? (si/no)").toLowerCase()
-
-}while (continuar != "si")
-
+remeras.forEach(remerasArray => {
+    divRemeras.innerHTML += `
+        <div class="card cardRemeras" style="width: 18rem;">
+            <div>
+                <img src="./multimedia/laUltimaCena.png" class="card-img-top imgRemeras" alt="...">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${remerasArray.modelo}</h5>
+                <p class="card-text">${remerasArray.precio}</p>
+                <a href="#" class="btn btn-success">COMPRAR</a>
+                <button id="agregar${remerasArray.id}" type="button" class="btn btn-secondary my-3">Agregar Carrito</button>
+            </div>
+        </div> 
+    `
+})
