35a207081d0ff36ad6955d7b364ebdf2115cede2
Martín Devita <martindevita23@gmail.com>
2022-08-28 22:12:31 -0300

    proyecto final (falta API)

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
 //1) creo clase Remera con sus caracterisiticas
-
 class Remeras {
-    constructor(id, modelo, talle, precio, stock, img) {
+    constructor(id, modelo, talle, precio, stock, cantidad, img) {
         this.id = id;
         this.modelo = modelo;
         this.talle = talle;
         this.precio = precio;
         this.stock = stock;
+        this.cantidad = cantidad
         this.img = img;
     }
 }
 
 
 //2) creo diferentes remeras y las guardo en el array "remeras"
-
-const remera1 = new Remeras (1, 'La creación', "XL", 4000, 10, "./multimedia/ultimaCena.png");
-const remera2 = new Remeras (2, 'La Última cena', "L", 4000, 5, "./multimedia/ultimaCena.png");
-const remera3 = new Remeras (3, 'La Noche Estrellada', "M", 4000, 8, "./multimedia/ultimaCena.png");
-const remera4 = new Remeras (4, 'La Joven de la Perla', "S", 4000, 15, "./multimedia/ultimaCena.png");
-const remera5 = new Remeras (5, 'El Nacimiento de Venus', "XS", 4000, 7, "./multimedia/nacimientoVenus.png");
-const remera6 = new Remeras (6, 'El Nacimiento de Venus', "XL", 4000, 6, "./multimedia/nacimientoVenus.png");
-const remera7 = new Remeras (7, 'El Nacimiento de Venus', "L", 4000, 4, "./multimedia/nacimientoVenus.png");
-const remera8 = new Remeras (8, 'El Nacimiento de Venus', "S", 4000, 6, "./multimedia/nacimientoVenus.png");
+const remera1 = new Remeras(1, 'La creación', "XL", 4001, 10, 1, "./multimedia/ultimaCena.png");
+const remera2 = new Remeras(2, 'La Última cena', "L", 4002, 5, 1, "./multimedia/ultimaCena.png");
+const remera3 = new Remeras(3, 'La Noche Estrellada', "M", 4003, 8, 1, "./multimedia/ultimaCena.png");
+const remera4 = new Remeras(4, 'La Joven de la Perla', "S", 4004, 15, 1, "./multimedia/ultimaCena.png");
+const remera5 = new Remeras(5, 'El Nacimiento de Venus', "XS", 4005, 7, 1, "./multimedia/nacimientoVenus.png");
+const remera6 = new Remeras(6, 'El Nacimiento de Venus', "XL", 4006, 6, 1, "./multimedia/nacimientoVenus.png");
+const remera7 = new Remeras(7, 'El Nacimiento de Venus', "L", 4007, 4, 1, "./multimedia/nacimientoVenus.png");
+const remera8 = new Remeras(8, 'El Nacimiento de Venus', "S", 4008, 6, 1, "./multimedia/nacimientoVenus.png");
 
 const remeras = [remera1, remera2, remera3, remera4, remera5, remera6, remera7, remera8];
 
@@ -29,14 +28,13 @@ localStorage.setItem("remeras", JSON.stringify([remeras]))
 
 
 //3) muestro las remeras en el DOM
-
 const contenedorRemeras = document.getElementById("contenedorRemeras");
 
 remeras.forEach(remera => {
     const divRemera = document.createElement('div');
-    divRemera.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12', 'cardMio');
+    divRemera.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12', 'cardMio', 'my-1', 'mx-0.5');
     divRemera.innerHTML = `
-        <div>
+        <div class="cardRem">
             <div id="${remera.id}">
                 <img src="${remera.img}" class="card-img-top img-fluid py-3" alt="...">
                 <div class="card-body">
@@ -56,45 +54,62 @@ remeras.forEach(remera => {
 
 
 // 4) creo un array vacío (carrito) para luego darle funacionalidad al boton de "añadir carrito"
-
 let carrito = [];
 
-const agregarAlCarrito = (id) => {
-    const item = remeras.find((remera) => remera.id === id);
-    carrito.push(item);
-    Toastify({
-        text: "Producto agregado al carrito",
-        duration: 3000,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-            background: "linear-gradient(to bottom left, #30655A, #0229BA)",
-        },
-        onClick: function(){
-            console.log("Diste click")
-        } // Callback after click
-    }).showToast();
 
-    console.log(carrito)
-    localStorage.setItem("carrito", JSON.stringify(carrito))
+// 5) localStorage del carrito una vez que el DOM se haya cargado
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("carrito")) {
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+        actuCarrito()
+    }
+})
+
+
+// 6) funcion agregar al carrito
+const agregarAlCarrito = (id) => {
+    const existe = carrito.some(prod => prod.id === id)
+
+    if (existe) {
+        const prod = carrito.map(prod => {
+            if (prod.id === id) {
+                prod.cantidad++
+            }
+        })
+    } else {
+        const item = remeras.find((remera) => remera.id === id);
+        carrito.push(item);
+        Toastify({
+            text: "Producto agregado al carrito",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to bottom left, #30655A, #0229BA)",
+            },
+            onClick: function () {
+                console.log("Diste click")
+            } // Callback after click
+        }).showToast();
+    }
+    actuCarrito()
 }
 
 
-// 5) muestro los productos añadidos al carrito y actualizo el mismo con una función
-
+// 7) muestro los productos añadidos al carrito y actualizo el mismo con una función
 const contenedorCarrito = document.getElementById("contCarrito")
 const updateCarrito = document.getElementById("botonCarrito")
 
 updateCarrito.addEventListener("click", actuCarrito)
 
-function actuCarrito(){
+function actuCarrito() {
     contenedorCarrito.innerHTML = ""
 
     let i = "";
     carrito.forEach(remera => {
-        i+=        
+        i +=
             `<div class="card text-bg-light mb-3" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4">
@@ -106,6 +121,7 @@ function actuCarrito(){
                             <p class="card-text"><span>$${remera.precio}</span></p>
                             <div>
                                 <p class="card-text"><small class="text-muted">Talle seleccionado: ${remera.talle}</small></p>
+                                <p>Cantidad:<span id="cantidad">${remera.cantidad}</span> </p>
                                 <button onClick = "eliminarProdCarrito(${remera.id})" class="btn btn-primary">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
@@ -117,19 +133,22 @@ function actuCarrito(){
                     </div>
                 </div>
             </div>`;
-        
-            contenedorCarrito.innerHTML = i;
+
+        contenedorCarrito.innerHTML = i;
+
     });
 
     contadorCarrito.innerText = carrito.length
     precioTotal.innerText = carrito.reduce((acu, prod) => acu + prod.precio, 0)
+    precioTotal2.innerText = carrito.reduce((acu, prod) => acu + prod.precio, 0)
+
+    localStorage.setItem("carrito", JSON.stringify(carrito))
 }
 
 
-// 6) creo una funcion para eliminar productos del carrito
-
+// 8) creo una funcion para eliminar productos del carrito y vaciarlo
 const eliminarProdCarrito = (remId) => {
-    const item  = carrito.find((rem) => rem.id === remId)
+    const item = carrito.find((rem) => rem.id === remId)
     const indice = carrito.indexOf(item)
     carrito.splice(indice, 1);
     actuCarrito()
@@ -138,11 +157,108 @@ const eliminarProdCarrito = (remId) => {
     localStorage.getItem(carrito)
 }
 
+const botonVaciar = document.getElementById("vaciarCarrito")
 
-// 7) constante del contador del carrito
+botonVaciar.addEventListener("click", () => {
+    carrito.length = 0
+    actuCarrito()
+})
+
+
+// 9) constante del contador del carrito
 const contadorCarrito = document.getElementById("contadorCarrito")
 
 
-// 8) constante precio total carrito
+// 10) constante precio total carrito
 const precioTotal = document.getElementById("precioTotal")
+const precioTotal2 = document.getElementById("precioTotal2")
+
+
+const botonFinalizarCompra = document.getElementById("btnFinalizar")
+
+botonFinalizarCompra.addEventListener("click", () => {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+        title: 'Desea finalizar su compra?',
+        text: "Revise nuevamente su carrito!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, finalizar compra',
+        cancelButtonText: 'No, seguir comprando',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+                'Compra finalizada',
+                'Muchas gracias por su compra, en breve nos pondremos en contacto para organizar la entrega',
+                'success'
+            )
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire(
+                'Perfecto!',
+                'Puede seguir comprando :)',
+                'error'
+            )
+        }
+    })
+})
+
+//filtrado de busqueda
+
+function buscadorInterno() {
+    filter = inputBusqueda.value.toLowerCase();
+    li = boxSearh.getElementById("inputBusqueda")
+}
+
+// document.addEventListener("keyup", e => {
+
+//     if (e.target.matches("inputBusqueda")) {
+//         document.querySelectorAll(".containerRems").forEach(rem => {
+//             rem.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+//             inner
+//         })
+//     }
+
+//     console.log(e.target.value)
+// })
+// //11) filtro de busqueda
+
+// const botonBusqueda = document.getElementById("botonBusqueda")
+
+// botonBusqueda.addEventListener("click", (evento) => {
+//     evento.preventDefault()
+//     busquedaHtml(inputBusqueda.value)
+// })
+
+// function busquedaHtml(inputBusqueda) {
+//     resultSearch = remeras.filter((remera) => remera.modelo.includes(inputBusqueda.toLowerCase()))
+//     showSearch(resultSearch)
+// }
+
+// function showSearch(resultSearch) {
+//     divRemeras.innerHTML = ``
+//     for (let remera of resultSearch) {
+//         divRemeras.innerHTML += `
+//         <div>
+//             <div id="${remera.id}">
+//                 <img src="${remera.img}" class="card-img-top img-fluid py-3" alt="...">
+//                 <div class="card-body">
+//                     <h3 class="card-title">${remera.modelo}</h3>
+//                     <p class="card-text">$${remera.precio}</p>
+//                     <button class="btn btn-primary">Agregar al Carrito</button>
+//                 </div>
+//             </div>
+//         </div>`
+//     }
+// }
 
